068b81c213cd703da6b8d71898dd61a43a5c094b
Bichi <sadananda.sahoo58@gmail.com>
2026-07-16 10:19:47 +0200

    Improve ICE server configuration for production

--- components/CallManager.jsx
+++ components/CallManager.jsx
@@ -5,14 +5,9 @@ const ICE_SERVERS = [
   { urls: 'stun:stun.l.google.com:19302' },
   { urls: 'stun:stun1.l.google.com:19302' },
   {
-    urls: 'turn:openrelay.metered.ca:80',
-    username: 'openrelayproject',
-    credential: 'openrelayproject',
-  },
-  {
-    urls: 'turn:openrelay.metered.ca:443',
-    username: 'openrelayproject',
-    credential: 'openrelayproject',
+    urls: ['turn:turnserver.studyo.app:3478'],
+    username: 'webrtc',
+    credential: 'webrtc',
   },
 ];
 
