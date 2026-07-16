fefbf01f088f3323e8d10dccc9bdb328c9c4ca65
Bichi <sadananda.sahoo58@gmail.com>
2026-07-16 09:43:16 +0200

    Add TURN server for better ICE connectivity

--- components/CallManager.jsx
+++ components/CallManager.jsx
@@ -4,6 +4,16 @@ import { usePresence } from '../lib/PresenceContext';
 const ICE_SERVERS = [
   { urls: 'stun:stun.l.google.com:19302' },
   { urls: 'stun:stun1.l.google.com:19302' },
+  {
+    urls: 'turn:openrelay.metered.ca:80',
+    username: 'openrelayproject',
+    credential: 'openrelayproject',
+  },
+  {
+    urls: 'turn:openrelay.metered.ca:443',
+    username: 'openrelayproject',
+    credential: 'openrelayproject',
+  },
 ];
 
 export default function CallManager({ profile, callRequest, onCallRequestHandled }) {
